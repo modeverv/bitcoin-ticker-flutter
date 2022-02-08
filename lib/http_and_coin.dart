a13ec9e7bcf10a366397d3de75eb39882ec47e0b
seijiro <modeverv@gmail.com>
2022-02-08 22:34:08 +0900

    2022-02-08 22:34:08

---
+++ lib/http_and_coin.dart
@@ -0,0 +1,41 @@
+import 'dart:convert' as convert;
+
+import 'package:http/http.dart' as http;
+
+//import 'dart:convert' as convert;
+//import 'package:http/http.dart' as http;
+/*
+curl https://rest.coinapi.io/v1/exchangerate/BTC/USD \
+  --request GET
+  --header "X-CoinAPI-Key: 73034021-THIS-IS-SAMPLE-KEY"
+The above command returns JSON structured like this:
+
+{
+  "time": "2017-08-09T14:31:18.3150000Z",
+  "asset_id_base": "BTC",
+  "asset_id_quote": "USD",
+  "rate": 3260.3514321215056208129867667
+}
+ */
+const u = 'https://rest.coinapi.io/v1/exchangerate/';
+const key = 'E41F5872-FE2B-4F3E-BB0E-E89AD26522A3';
+
+class HttpCoinGet {
+  Future<double> get(String from, String to, Function f) async {
+    var url = Uri.parse(u + from + '/' + to + '/?apiKey=$key');
+    print(url);
+    var rate = 0.0;
+    http.Response response = await http.get(url);
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      print(jsonResponse);
+      rate = jsonResponse['rate'];
+      f(rate);
+      return rate;
+    } else {
+      return rate;
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:bitcoin_ticker/http_and_coin.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +12,16 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  String pullDownValue = currenciesList.first;
+  String currency = currenciesList.first;
+  String btc = '3000';
+  String eth = '4000';
+  String ltc = '5000';
+
+  @override
+  void initState() {
+    super.initState();
+    setVirtualMoneyValue();
+  }
 
   List<DropdownMenuItem> getPullDownItems() {
     return currenciesList
@@ -18,6 +30,86 @@ class _PriceScreenState extends State<PriceScreen> {
               value: e,
             ))
         .toList();
+  }
+
+  HttpCoinGet myApi = HttpCoinGet();
+
+  void setVirtualMoneyValue() async {
+    print(currency);
+    /*
+    await Future.wait([
+      myApi.get('BTC', currency, (double rate) {
+        setState(() {
+          print('BTC - $currency - $rate');
+          this.btc = rate.toStringAsFixed(0);
+        });
+      }),
+      myApi.get('ETH', currency, (double rate) {
+        setState(() {
+          print('ETH - $currency - $rate');
+          this.eth = rate.toStringAsFixed(0);
+        });
+      }),
+      myApi.get('LTC', currency, (double rate) {
+        setState(() {
+          print('LTC - $currency - $rate');
+          this.ltc = rate.toStringAsFixed(0);
+        });
+      }),
+    ]);
+    */
+    await myApi.get('BTC', currency, (double rate) {
+      setState(() {
+        print('BTC - $currency - $rate');
+        this.btc = rate.toStringAsFixed(0);
+      });
+    });
+    await myApi.get('ETH', currency, (double rate) {
+      setState(() {
+        print('ETH - $currency - $rate');
+        this.eth = rate.toStringAsFixed(0);
+      });
+    });
+    await myApi.get('LTC', currency, (double rate) {
+      setState(() {
+        print('LTC - $currency - $rate');
+        this.ltc = rate.toStringAsFixed(0);
+      });
+    });
+    print('done');
+  }
+
+  void setIOSSelect2Currency(int index) {
+    setState(() {
+      currency = currenciesList[index];
+      print(currency);
+    });
+  }
+
+  Widget getPicker() {
+    if (Platform.isIOS) {
+      return CupertinoPicker(
+        itemExtent: 32.0,
+        children: currenciesList.map((e) => Text(e)).toList(),
+        backgroundColor: Colors.lightBlue,
+        onSelectedItemChanged: (selected) {
+          setIOSSelect2Currency(selected);
+          print(selected);
+          setVirtualMoneyValue();
+        },
+      );
+    } else {
+      return DropdownButton(
+        value: currency,
+        items: getPullDownItems(),
+        onChanged: (value) {
+          setState(() {
+            currency = value;
+            setVirtualMoneyValue();
+          });
+        },
+      );
+    }
   }
 
   @override
@@ -32,50 +124,77 @@ class _PriceScreenState extends State<PriceScreen> {
         children: <Widget>[
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Card(
+                  color: Colors.lightBlueAccent,
+                  elevation: 5.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                    child: Text(
+                      '1 BTC = $btc $currency',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                Card(
+                  color: Colors.lightBlueAccent,
+                  elevation: 5.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                    child: Text(
+                      '1 ETH = $eth $currency',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.lightBlueAccent,
+                  elevation: 5.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                    child: Text(
+                      '1 LTC = $ltc $currency',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: CupertinoPicker(
-              itemExtent: 32.0,
-              children: currenciesList.map((e) => Text(e)).toList(),
-              backgroundColor: Colors.lightBlue,
-              onSelectedItemChanged: (selected) {
-                print(selected);
-              },
-            ),
-          ),
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: getPicker()),
         ],
       ),
     );
   }
 }
-//DropdownButton(
-//value: pullDownValue,
-//items: getPullDownItems(),
-//onChanged: (value) {
-//setState(() {
-//pullDownValue = value;
-//});
-//},
-//),
